5d7c49645b0a8de7db3f986d2c0df8cf7a6347d7
hj6110195-stack <hj6110195@gmail.com>
2026-09-05 07:41:18 +0000

    comparision.js

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -24,6 +24,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let SomeNumber = 33
 
 let StringNumber = String(SomeNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+// ***************************************** Operations ***************************************** 
+
+let value = 5
+let negative = -value
+console.log(negative);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "Anant"
+// let str2 = " jain"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(10 + "2");
+// console.log("1" + "2" + "2");
+
+// console.log((3+4) * (3/30));
+
+// console.log(true);
+// console.log(+"");
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 200
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
 
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,30 @@
+// console.log(10 > 5);
+// console.log(10 >= 5);
+// console.log(10 < 5);
+// console.log(10 == 5);
+// console.log(10 != 5);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0 );
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" == 2);
+
+
+
+
+
+
+
+
+
