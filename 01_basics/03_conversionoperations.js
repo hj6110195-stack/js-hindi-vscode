cfd9109eb4d481678c2031d1da5851446315448b
hj6110195-stack <hj6110195@gmail.com>
2026-09-05 05:43:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,29 @@
+let score = "Harsh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1; false => 0
+
+let isLoggedIn = "Harsh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false;
+// "" => false
+// "harsh" => true
+
+let SomeNumber = 33
+
+let StringNumber = String(SomeNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
