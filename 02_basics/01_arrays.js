20f12aae58986a24897f032c800269a64c1c1064
hj6110195-stack <hj6110195@gmail.com>
2026-09-16 03:53:01 +0000

    02_basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,5 @@
+// array
+const myArr = [0, 1, 2, 3, 4, 5, 6]
+const myHeros = ["shaktiman", "sharukhkhan", "salmaan khan", "Harsh vardhan"]
+const myArr2 = new Array(1, 2, 3, 4, 5)
+console.log(myArr[0]);
