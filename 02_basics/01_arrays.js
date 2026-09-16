a2d668e1ac3e5af644d62420b61d9e1483cdf0cf
hj6110195-stack <hj6110195@gmail.com>
2026-09-16 04:32:37 +0000

    arrays.js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,4 +2,37 @@
 const myArr = [0, 1, 2, 3, 4, 5, 6]
 const myHeros = ["shaktiman", "sharukhkhan", "salmaan khan", "Harsh vardhan"]
 const myArr2 = new Array(1, 2, 3, 4, 5)
-console.log(myArr[0]);
+// console.log(myArr2[2]);
+
+
+// Array methods
+
+// myArr.push(5)
+// myArr.push(7)
+// myArr.push()
+
+// myArr.unshift(9)
+myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(newArr);
+
+// console.log(myArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 5)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 5)
+console.log("C", myArr);
+
+console.log(myn2);
